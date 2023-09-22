17d4ea9d7a75e7e9241183bd949165c6ba6c8365
HubHed16 <hubert.hdn@gmail.com>
2023-09-22 15:08:48 +0200

    new dbconnect

--- controller/recoverCGquiz.php
+++ controller/recoverCGquiz.php
@@ -1,25 +1,28 @@
 <?php
-require('dbConnect.php');
+// Inclure le fichier de connexion
+require_once('dbConnect.php');
 
 try {
-    // Requête SQL pour récupérer toutes les données de la table culture_generale
+    // Préparer et exécuter une requête SQL pour récupérer les données de la table culture_generale
     $query = "SELECT * FROM culture_generale";
+    $stmt = $dbh->query($query);
 
-    // Exécution de la requête
-    $stmt = $pdo->query($query);
-
-    // Boucle pour parcourir les résultats
+    // Parcourir les résultats
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        // Utilisez $row pour accéder aux données de chaque ligne
-        echo "Question : " . $row['question'] . "<br>";
-        echo "Réponse : " . $row['reponse'] . "<br>";
-        echo "<br>";
+        // Traitez ici les données de chaque ligne, par exemple :
+        $question = $row['question'];
+        $reponse = $row['reponse'];
+
+        // Faites ce que vous voulez avec les données (affichage, stockage, etc.)
+        echo "Question : $question<br>";
+        echo "Réponse : $reponse<br>";
+        echo "<hr>";
     }
+
+    // Fermer la connexion
+    $dbh = null;
 } catch (PDOException $e) {
     // En cas d'erreur
-    echo "Erreur : " . $e->getMessage();
+    echo 'Erreur : ' . $e->getMessage();
 }
-
-
-
 ?>
