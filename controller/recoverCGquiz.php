2d08c8c2ba1f73d89297d77cab5edd273952ce09
HubHed16 <hubert.hdn@gmail.com>
2023-09-22 14:47:01 +0200

    recover quiz

--- controller/recoverCGquiz.php
+++ controller/recoverCGquiz.php
@@ -1,3 +1,25 @@
 <?php
 require('dbConnect.php');
+
+try {
+    // Requête SQL pour récupérer toutes les données de la table culture_generale
+    $query = "SELECT * FROM culture_generale";
+
+    // Exécution de la requête
+    $stmt = $pdo->query($query);
+
+    // Boucle pour parcourir les résultats
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        // Utilisez $row pour accéder aux données de chaque ligne
+        echo "Question : " . $row['question'] . "<br>";
+        echo "Réponse : " . $row['reponse'] . "<br>";
+        echo "<br>";
+    }
+} catch (PDOException $e) {
+    // En cas d'erreur
+    echo "Erreur : " . $e->getMessage();
+}
+
+
+
 ?>
---
+++ pages/theme1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thème</title>
+    <link rel="stylesheet" type="text/css" href="../css/theme1.css">
+</head>
+<body>
+<h1>Thème Culture Générale</h1>
+
+<!-- Inclure les données récupérées ici -->
+<?php include('../controller/recoverCGquiz.php'); ?>
+
+</body>
+</html>
